3f4bf92f3c3a27c201211afa8334855affa0f9d1
Chris Kaiser <cgk2130@columbia.edu>
2019-07-24 16:01:20 -0400

    hh

---
+++ Classwork.py
@@ -0,0 +1,18 @@
+from random import randint
+
+x = input("Guess a number between 1 and 5")
+x = int (x)
+y = randint (1,5)
+print (y)
+correct = False
+if (x==y):
+    print ("You win!")
+while (correct == False):
+    print ("Try again")
+    x = input("Pick a new number:")
+    x = int (x)
+    if (x==y):
+        correct = True
+        print ("You win")
+    
+
